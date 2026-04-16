3aa3528e25a27d796dd5c6e243fd9337d9cfb6d8
chloe <chloe@172.17.106.63>
2026-04-16 11:45:51 +0200

    Package controleur

---
+++ src/main/java/Controleur/CBatiment.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controleur;
+
+import javafx.scene.Scene;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import Vue.VueBatiment;
+import Modele.Batiment;
+import Modele.Stockage;
+
+
+/**
+ *
+ * @author chloe
+ */
+
+public class CBatiment {
+
+    private VueBatiment vue;
+    private Stage fenetre;
+
+    private String typeBatiment;
+
+    public CBatiment(Stage fenetre) {
+
+        this.fenetre = fenetre;
+
+        vue = new VueBatiment();
+
+        // clic Maison
+        vue.getMaison().setOnAction(e -> {
+            typeBatiment = "Maison";
+            afficherSuperficie();
+        });
+
+        // clic Immeuble
+        vue.getImmeuble().setOnAction(e -> {
+            typeBatiment = "Immeuble";
+            afficherSuperficie();
+        });
+
+        // bouton valider
+        vue.getValider().setOnAction(e -> {
+
+    // on récupère ce que l'utilisateur a écrit
+    String texteSuperficie = vue.getChampSuperficie().getText();
+
+    // conversion en nombre
+    float superficie = Float.parseFloat(texteSuperficie);
+
+    // création du bâtiment avec le type déjà choisi + superficie
+    Batiment batiment = new Batiment(typeBatiment, superficie);
+
+    // on stocke le bâtiment dans la liste globale
+    Stockage.batiments.add(batiment);
+
+    // debug console (pour vérifier)
+    System.out.println("Bâtiment créé !");
+    System.out.println("Type : " + batiment.getTypeBatiment());
+    System.out.println("Superficie : " + batiment.getSuperficie());
+});
+
+    }
+
+    private void afficherSuperficie() {
+
+        vue.getLabelSuperficie().setVisible(true);
+        vue.getChampSuperficie().setVisible(true);
+        vue.getValider().setVisible(true);
+    }
+}
